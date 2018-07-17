755c3d819015a0748507a967afec5db3fec6e227
Michel Bourgeois <michel.bourgeois114@gmail.com>
2018-07-17 18:14:30 -0300

    Add all files

---
+++ TextEditor/Form1.Designer.cs
@@ -0,0 +1,266 @@
+﻿namespace TextEditor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.loadToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.settingsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.colorToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.fontColorToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.backgroundColorToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.fontToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.richTextBox = new System.Windows.Forms.RichTextBox();
+            this.fileNameLabel = new System.Windows.Forms.Label();
+            this.paragraphsValueLabel = new System.Windows.Forms.Label();
+            this.paragraphsTextLabel = new System.Windows.Forms.Label();
+            this.wordsValueLabel = new System.Windows.Forms.Label();
+            this.wordsTextLabel = new System.Windows.Forms.Label();
+            this.charactersValueLabel = new System.Windows.Forms.Label();
+            this.charactersTextLabel = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.menuStrip.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip
+            // 
+            this.menuStrip.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.menuStrip.Dock = System.Windows.Forms.DockStyle.None;
+            this.menuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.settingsToolStripMenuItem});
+            this.menuStrip.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip.Name = "menuStrip";
+            this.menuStrip.Size = new System.Drawing.Size(106, 24);
+            this.menuStrip.TabIndex = 1;
+            this.menuStrip.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.saveToolStripMenuItem,
+            this.loadToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(100, 22);
+            this.saveToolStripMenuItem.Text = "Save";
+            this.saveToolStripMenuItem.Click += new System.EventHandler(this.saveToolStripMenuItem_Click);
+            // 
+            // loadToolStripMenuItem
+            // 
+            this.loadToolStripMenuItem.Name = "loadToolStripMenuItem";
+            this.loadToolStripMenuItem.Size = new System.Drawing.Size(100, 22);
+            this.loadToolStripMenuItem.Text = "Load";
+            this.loadToolStripMenuItem.Click += new System.EventHandler(this.loadToolStripMenuItem_Click);
+            // 
+            // settingsToolStripMenuItem
+            // 
+            this.settingsToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.colorToolStripMenuItem,
+            this.fontToolStripMenuItem});
+            this.settingsToolStripMenuItem.Name = "settingsToolStripMenuItem";
+            this.settingsToolStripMenuItem.Size = new System.Drawing.Size(61, 20);
+            this.settingsToolStripMenuItem.Text = "Settings";
+            // 
+            // colorToolStripMenuItem
+            // 
+            this.colorToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fontColorToolStripMenuItem,
+            this.backgroundColorToolStripMenuItem});
+            this.colorToolStripMenuItem.Name = "colorToolStripMenuItem";
+            this.colorToolStripMenuItem.Size = new System.Drawing.Size(103, 22);
+            this.colorToolStripMenuItem.Text = "Color";
+            // 
+            // fontColorToolStripMenuItem
+            // 
+            this.fontColorToolStripMenuItem.Name = "fontColorToolStripMenuItem";
+            this.fontColorToolStripMenuItem.Size = new System.Drawing.Size(170, 22);
+            this.fontColorToolStripMenuItem.Text = "Font Color";
+            this.fontColorToolStripMenuItem.Click += new System.EventHandler(this.fontColorToolStripMenuItem_Click);
+            // 
+            // backgroundColorToolStripMenuItem
+            // 
+            this.backgroundColorToolStripMenuItem.Name = "backgroundColorToolStripMenuItem";
+            this.backgroundColorToolStripMenuItem.Size = new System.Drawing.Size(170, 22);
+            this.backgroundColorToolStripMenuItem.Text = "Background Color";
+            this.backgroundColorToolStripMenuItem.Click += new System.EventHandler(this.backgroundColorToolStripMenuItem_Click);
+            // 
+            // fontToolStripMenuItem
+            // 
+            this.fontToolStripMenuItem.Name = "fontToolStripMenuItem";
+            this.fontToolStripMenuItem.Size = new System.Drawing.Size(103, 22);
+            this.fontToolStripMenuItem.Text = "Font";
+            this.fontToolStripMenuItem.Click += new System.EventHandler(this.fontToolStripMenuItem_Click);
+            // 
+            // richTextBox
+            // 
+            this.richTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.richTextBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.richTextBox.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.richTextBox.Location = new System.Drawing.Point(0, 24);
+            this.richTextBox.Name = "richTextBox";
+            this.richTextBox.Size = new System.Drawing.Size(756, 434);
+            this.richTextBox.TabIndex = 2;
+            this.richTextBox.Text = "";
+            this.richTextBox.KeyDown += new System.Windows.Forms.KeyEventHandler(this.richTextBox_KeyDown);
+            this.richTextBox.KeyUp += new System.Windows.Forms.KeyEventHandler(this.richTextBox_KeyUp);
+            // 
+            // fileNameLabel
+            // 
+            this.fileNameLabel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.fileNameLabel.AutoSize = true;
+            this.fileNameLabel.Location = new System.Drawing.Point(652, 4);
+            this.fileNameLabel.Name = "fileNameLabel";
+            this.fileNameLabel.Size = new System.Drawing.Size(50, 13);
+            this.fileNameLabel.TabIndex = 6;
+            this.fileNameLabel.Text = "No name";
+            // 
+            // paragraphsValueLabel
+            // 
+            this.paragraphsValueLabel.AutoSize = true;
+            this.paragraphsValueLabel.Location = new System.Drawing.Point(305, 4);
+            this.paragraphsValueLabel.Name = "paragraphsValueLabel";
+            this.paragraphsValueLabel.Size = new System.Drawing.Size(13, 13);
+            this.paragraphsValueLabel.TabIndex = 5;
+            this.paragraphsValueLabel.Text = "0";
+            // 
+            // paragraphsTextLabel
+            // 
+            this.paragraphsTextLabel.AutoSize = true;
+            this.paragraphsTextLabel.Location = new System.Drawing.Point(235, 4);
+            this.paragraphsTextLabel.Name = "paragraphsTextLabel";
+            this.paragraphsTextLabel.Size = new System.Drawing.Size(64, 13);
+            this.paragraphsTextLabel.TabIndex = 4;
+            this.paragraphsTextLabel.Text = "Paragraphs:";
+            // 
+            // wordsValueLabel
+            // 
+            this.wordsValueLabel.AutoSize = true;
+            this.wordsValueLabel.Location = new System.Drawing.Point(180, 4);
+            this.wordsValueLabel.Name = "wordsValueLabel";
+            this.wordsValueLabel.Size = new System.Drawing.Size(13, 13);
+            this.wordsValueLabel.TabIndex = 3;
+            this.wordsValueLabel.Text = "0";
+            // 
+            // wordsTextLabel
+            // 
+            this.wordsTextLabel.AutoSize = true;
+            this.wordsTextLabel.Location = new System.Drawing.Point(133, 4);
+            this.wordsTextLabel.Name = "wordsTextLabel";
+            this.wordsTextLabel.Size = new System.Drawing.Size(41, 13);
+            this.wordsTextLabel.TabIndex = 2;
+            this.wordsTextLabel.Text = "Words:";
+            // 
+            // charactersValueLabel
+            // 
+            this.charactersValueLabel.AutoSize = true;
+            this.charactersValueLabel.Location = new System.Drawing.Point(72, 4);
+            this.charactersValueLabel.Name = "charactersValueLabel";
+            this.charactersValueLabel.Size = new System.Drawing.Size(13, 13);
+            this.charactersValueLabel.TabIndex = 1;
+            this.charactersValueLabel.Text = "0";
+            // 
+            // charactersTextLabel
+            // 
+            this.charactersTextLabel.AutoSize = true;
+            this.charactersTextLabel.Location = new System.Drawing.Point(5, 4);
+            this.charactersTextLabel.Name = "charactersTextLabel";
+            this.charactersTextLabel.Size = new System.Drawing.Size(61, 13);
+            this.charactersTextLabel.TabIndex = 0;
+            this.charactersTextLabel.Text = "Characters:";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.fileNameLabel);
+            this.panel1.Controls.Add(this.paragraphsValueLabel);
+            this.panel1.Controls.Add(this.paragraphsTextLabel);
+            this.panel1.Controls.Add(this.wordsValueLabel);
+            this.panel1.Controls.Add(this.wordsTextLabel);
+            this.panel1.Controls.Add(this.charactersValueLabel);
+            this.panel1.Controls.Add(this.charactersTextLabel);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel1.Location = new System.Drawing.Point(0, 457);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(756, 23);
+            this.panel1.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(756, 480);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.richTextBox);
+            this.Controls.Add(this.menuStrip);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.MainMenuStrip = this.menuStrip;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.menuStrip.ResumeLayout(false);
+            this.menuStrip.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.MenuStrip menuStrip;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem loadToolStripMenuItem;
+        private System.Windows.Forms.RichTextBox richTextBox;
+        private System.Windows.Forms.Label charactersValueLabel;
+        private System.Windows.Forms.Label charactersTextLabel;
+        private System.Windows.Forms.Label wordsValueLabel;
+        private System.Windows.Forms.Label wordsTextLabel;
+        private System.Windows.Forms.Label paragraphsValueLabel;
+        private System.Windows.Forms.Label paragraphsTextLabel;
+        private System.Windows.Forms.ToolStripMenuItem settingsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem colorToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem fontToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem fontColorToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem backgroundColorToolStripMenuItem;
+        private System.Windows.Forms.Label fileNameLabel;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
---
+++ TextEditor/Form1.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace TextEditor
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void saveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+
+            saveFileDialog.Filter = "rtf files (*.rtf)|*.rtf";
+            saveFileDialog.FilterIndex = 2;
+            saveFileDialog.RestoreDirectory = true;
+
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                richTextBox.SaveFile(saveFileDialog.FileName, RichTextBoxStreamType.RichText);
+                string fileName = Path.GetFileName(saveFileDialog.FileName);
+                fileNameLabel.Text = fileName;
+            }
+        }
+
+        private void loadToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+
+            openFileDialog.Filter = "rtf files (*.rtf)|*.rtf";
+            openFileDialog.FilterIndex = 2;
+            openFileDialog.RestoreDirectory = true;
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                richTextBox.LoadFile(openFileDialog.FileName);
+                string fileName = Path.GetFileName(openFileDialog.FileName);
+                fileNameLabel.Text = fileName;
+            }
+        }
+
+        private void richTextBox_KeyUp(object sender, KeyEventArgs e)
+        {
+            charactersValueLabel.Text = richTextBox.Text.Length.ToString();
+            wordsValueLabel.Text = FindWordCount().ToString();
+            paragraphsValueLabel.Text = FindParagraphCount().ToString();
+            if (richTextBox.Text.Length == 0)
+            {
+                wordsValueLabel.Text = "0";
+                paragraphsValueLabel.Text = "0";
+            }
+        }
+
+        private int FindWordCount()
+        {
+            int wordCount = 1;
+            string text = richTextBox.Text;
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (text[i] == ' ' && i != text.Length - 1 && text[i + 1] != ' ')
+                {
+                    wordCount++;
+                }
+                else if (text[i] != ' ' && i != text.Length - 1 && i > 0 && text[i - 1] == '\n')
+                {
+                    wordCount++;
+                }
+
+            }
+            return wordCount;
+        }
+
+        private int FindParagraphCount()
+        {
+            int paragraphCount = 1;
+            string text = richTextBox.Text;
+            foreach (char character in text)
+            {
+                if (character == '\n')
+                {
+                    paragraphCount++;
+                }
+            }
+            return paragraphCount;
+        }
+
+        private void richTextBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Control && e.KeyCode == Keys.B)
+            {
+                if (richTextBox.SelectionFont.Style == FontStyle.Bold)
+                {
+                    richTextBox.SelectionFont = new Font(richTextBox.Font, FontStyle.Regular);
+                }
+                else
+                {
+                    richTextBox.SelectionFont = new Font(richTextBox.Font, FontStyle.Bold);
+                }
+            }
+            else if (e.Control && e.KeyCode == Keys.I)
+            {
+                if (richTextBox.SelectionFont.Style == FontStyle.Italic)
+                {
+                    richTextBox.SelectionFont = new Font(richTextBox.Font, FontStyle.Regular);
+                    e.SuppressKeyPress = true;
+                }
+                else
+                {
+                    richTextBox.SelectionFont = new Font(richTextBox.Font, FontStyle.Italic);
+                    e.SuppressKeyPress = true;
+                }
+            }
+            else if (e.Control && e.KeyCode == Keys.S)
+            {
+                saveToolStripMenuItem.PerformClick();
+            }
+            else if (e.Control && e.KeyCode == Keys.O)
+            {
+                loadToolStripMenuItem.PerformClick();
+            }
+        }
+
+        private void fontColorToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ColorDialog colorDialog = new ColorDialog();
+            if (colorDialog.ShowDialog() == DialogResult.OK)
+            {
+                richTextBox.ForeColor = colorDialog.Color;
+                charactersTextLabel.ForeColor = colorDialog.Color;
+                charactersValueLabel.ForeColor = colorDialog.Color;
+                wordsTextLabel.ForeColor = colorDialog.Color;
+                wordsValueLabel.ForeColor = colorDialog.Color;
+                paragraphsTextLabel.ForeColor = colorDialog.Color;
+                paragraphsValueLabel.ForeColor = colorDialog.Color;
+                menuStrip.ForeColor = colorDialog.Color;
+            }
+        }
+
+        private void backgroundColorToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ColorDialog colorDialog = new ColorDialog();
+            if (colorDialog.ShowDialog() == DialogResult.OK)
+            {
+                richTextBox.BackColor = colorDialog.Color;
+                menuStrip.BackColor = colorDialog.Color;
+                panel1.BackColor = colorDialog.Color;
+                this.BackColor = colorDialog.Color;
+            }
+        }
+
+        private void fontToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            FontDialog fontDialog = new FontDialog();
+            if (fontDialog.ShowDialog() == DialogResult.OK)
+            {
+                richTextBox.Font = fontDialog.Font;
+            }
+        }
+    }
+}
